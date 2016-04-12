2ded0822ba1a0c38bc74d45266684687b43b05f9
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-12 10:25:26 -0500

    Add validation code

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,23 @@
-puts "What is the hamster's name"
-h_name = gets.chomp
-puts "How would you rate your level of sleeping (1 to 10)"
-volume = gets.chomp
+h_name = ""
+while h_name == "" do 
+  puts "What is the hamster's name"
+  h_name = gets.chomp
+end
+
+
+vol_check = 0
+while vol_check < 1 || vol_check > 10 
+  puts "How would you rate your level of sleeping (1 to 10)"
+  volume = gets.chomp
+  vol_check = volume.to_i
+end
+
+
 puts "Fur Color"
 fur = gets.chomp
 puts "Is the hamster a good candidate for adoption (y/n)"
 candidate = gets.chomp
+
 puts "Estimated age"
 age = gets.chomp
 
