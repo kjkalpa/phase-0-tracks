d2012bf22895fe6b92f37486d35f5cf1ea32a376
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-12 11:15:37 -0500

    Complete assignment

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,27 +1,51 @@
+
 h_name = ""
 while h_name == "" do 
-  puts "What is the hamster's name"
+  puts "What is the hamster's name?"
   h_name = gets.chomp
 end
 
-
 vol_check = 0
 while vol_check < 1 || vol_check > 10 
-  puts "How would you rate your level of sleeping (1 to 10)"
+  puts "How would you rate your level of sleeping? (1 to 10)"
   volume = gets.chomp
   vol_check = volume.to_i
 end
 
+fur = ""
+while fur == ""
+  puts "Fur Color"
+  fur = gets.chomp
+end
 
-puts "Fur Color"
-fur = gets.chomp
-puts "Is the hamster a good candidate for adoption (y/n)"
-candidate = gets.chomp
+candidate = ""
+while candidate.downcase != "y" && candidate.downcase != "n"
+  puts "Is the hamster a good candidate for adoption? (y/n)"
+  candidate = gets.chomp
+  candidate.downcase! 
+end 
 
-puts "Estimated age"
+puts "Estimated age?"
 age = gets.chomp
 
 if age == ""
   age = nil
+else 
+  age = age.to_i
+  p age
 end
 
+puts "Your hamster's name is #{h_name}!"
+puts "You rated your level of sleeping as a #{vol_check}"
+puts "Your hamster is #{fur}"
+if candidate == "y"
+  puts "Your hamster is a good candidate for adoption"
+else
+  puts "Your hamster is a poor candidate for adoption"
+end
+
+if age == nil
+  puts "We don't know the hamster's age."
+else
+  puts "Your hamster is #{age} years old!"
+end
