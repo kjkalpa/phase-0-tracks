f829efeed8b042d5578933dfb99eff43dc620fa0
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-13 12:11:10 -0500

    Add code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,4 +9,18 @@ Do something with Z
 print encrypted word
 =end
 
+  for x in 0...phrase.length
+    if phrase[x] == " "
+      print " "
+    else
+      print phrase[x].next[0]
+    end
+  end
+end
 
+
+
+print "Enter a word to encrypt: "
+word = gets.chomp
+
+encrypt word
