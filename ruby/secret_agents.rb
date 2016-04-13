de66c00e0c58a6139e3c7b793df3ca18972ccc2a
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-13 12:10:52 -0500

    Add psuedo code

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,12 @@
+def encrypt (phrase)
+=begin
+Encrypt Method:
+Take string input from user 
+Index through string through length of the string
+Move one letter forward in the alphabet for each letter in the string
+Ignore spaces 
+Do something with Z
+print encrypted word
+=end
+
+
