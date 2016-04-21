6d63211e2fedf44051db0c827cafa12cda614d80
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-20 20:05:56 -0500

    Test file

--- ruby/kk.rb
+++ ruby/kk.rb
@@ -1,58 +1,27 @@
-clear_code = %x{clear}  #"\e[H\e[2J"
+letters = ["a", "b", "c", "d", "e"]
+new_letters = []
 
-h_name = ""
-while h_name == "" do 
-  puts "What is the hamster's name?"
-  h_name = gets.chomp
+puts "Original data: "
+p letters
+p new_letters
+
+
+letters.each do |letter|
+  new_letters << letter.next
 end
 
-vol_check = 0
-until vol_check >= 1 && vol_check <= 10 
-  puts "How would you rate your level of sleeping? (1 to 10)"
-  volume = gets.chomp
-  volume.upcase!
-  vol_check = volume.to_i
-  p volume
+puts "After .each call:"
+p letters
+p new_letters
+
+
+numbers = {1 => 'one', 2 => 'two', 3 => 'three'}
+
+numbers.each do |digit, word| 
+  puts "#{digit} is spelled out as #{word}"
 end
 
-fur = ""
-while fur == ""
-  puts "Fur Color"
-  fur = gets.chomp
-end
 
-candidate = ""
-while candidate.downcase != "y" && candidate.downcase != "n"
-  puts "Is the hamster a good candidate for adoption? (y/n)"
-  candidate = gets.chomp
-  candidate.downcase! 
-end 
 
-puts "Estimated age?"
-age = gets.chomp
-
-if age == ""
-  age = nil
-else 
-  age = age.to_i
-  p age
-end
-
-print clear_code
-
-puts "Your hamster's name is #{h_name}!"
-puts "You rated your level of sleeping as a #{vol_check}"
-puts "Your hamster is #{fur}"
-if candidate == "y"
-  puts "Your hamster is a good candidate for adoption"
-else
-  puts "Your hamster is a poor candidate for adoption"
-end
-
-if age == nil
-  puts "We don't know the hamster's age."
-else
-  puts "Your hamster is #{age} years old!"
-end
 
 
