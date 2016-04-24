e79f881bc7b668c54c01e2d22a425e253ccb07a1
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-24 11:30:43 -0500

    Add pseudo code

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,18 @@
+
+=begin
+Take name 
+Swap first and last name
+  Split name(string) at space 
+  Create a new array with two items in it (first and last name)
+  Reverse the order of the name
+  Create a name again(string)
+
+Cycle through all the letters of the name and substitute the next letter 
+alphabet.
+  Check if current letter is a vowel
+    If it is then loop through next letters until next letter is a vowel
+
+    If it isn't (ELSE) then loop through next letters until next letter is a consonant
+
+  Assign letter selected to array
+=end  
