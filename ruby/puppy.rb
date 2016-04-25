d1fbf6bf3804510afc2f77234d24c92ca5e6df6e
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-25 17:09:59 -0500

    Initial Commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+  #Characteristics - Attributes
+    # Two eyes
+    # Four legs
+    # Cold nose
+    # Tail
+    # Cuteness factor
+    # Fur
+    # Loyalty
+
+  #Behaviors - Methods
+    # Play dead
+    # Roll over
+    # Give paw
+    # Bark
+    # Growl
+    # Jump
+    
+
+ 
