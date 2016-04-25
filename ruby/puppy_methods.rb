3d8905b0a0c6b6c2b0a55498c7dfa27b93a788b5
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-25 17:04:53 -0500

    Add pseudocode for methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,28 @@
 class Puppy
 
+  #Characteristics - Attributes
+    # Two eyes
+    # Four legs
+    # Cold nose
+    # Tail
+    # Cuteness factor
+    # Fur
+    # Loyalty
+
+  #Behaviors - Methods
+    # Play dead
+    # Roll over
+    # Give paw
+    # Bark
+    # Growl
+    # Jump
+    
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
 end
+
 
