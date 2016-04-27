dd896c14673ffa50e5bf2aa9ba5366b4d857cc99
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-27 09:50:58 -0500

    Add random age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,9 @@
 class Santa  
-  attr_reader :age, :ethnicity
-  attr_accessor :gender
+  attr_reader  :ethnicity
+  attr_accessor :gender, :age
+
   def initialize(gender, ethnicity)
-    puts "Initializing Santa instance"
+    #puts "Initializing Santa instance"
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
@@ -42,6 +43,9 @@ class Santa
 
 end
 
+##### Driver Code
+prng = Random.new
+
 # Kris = Santa.new
 # Kris.eat_milk_and_cookies("Chocolate Chip cookie")
 
@@ -51,23 +55,34 @@ santas = []
 example_genders = ["Androgyne","Bigender","FTM","Gender Nonconforming","Intersex","MTF","Non-binary","Pangender"]
 
 # example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_ethnicities = ["Eurasian", "Klingon", "Black", "White", "Latino", "None of your business", "Asian", "Hawaiian"]
+example_ethnicities = ["Eurasian", "European", "Black", "White", "Latino", "None of your business", "Asian", "Hawaiian"]
 
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+#example_genders.length.times do |i|
+num_of_santas = 350
+
+num_of_santas.times do |i|
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+  santas[i].age = prng.rand(0..140)
+  puts "Santa # #{i+1} is a #{santas[i].gender}, #{santas[i].ethnicity}, #{santas[i].age} years old"
 end
   
-santas[0].celebrate_birthday
-p santas[0]
-
+# p santas[0]
 # ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
    
 santas[0].get_mad_at("Prancer")
-p santas[0]
+# p santas[0]
 santas[0].gender ="No Gender"
-p santas[0]
-p santas[0].age
-p santas[0].ethnicity
+# p santas[0]
+
+santas[-1].celebrate_birthday
+puts "---------------------"
+puts "The last Santa's new age is #{santas[-1].age}"
+puts "The last Sants's ethnicity is #{santas[-1].ethnicity}"
+
+
+
+
+
 
 
 
