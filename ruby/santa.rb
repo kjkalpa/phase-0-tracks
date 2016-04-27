1afd3412d9290d778da541cfbf89f817242b799d
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-27 16:58:50 -0500

    Refactor code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -52,16 +52,16 @@ prng = Random.new
 santas = []
 
 # example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_genders = ["Androgyne","Bigender","FTM","Gender Nonconforming","Intersex","MTF","Non-binary","Pangender"]
+gender_list = ["Androgyne","Bigender","FTM","Gender Nonconforming","Intersex","MTF","Non-binary","Pangender"]
 
 # example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_ethnicities = ["Eurasian", "European", "Black", "White", "Latino", "None of your business", "Asian", "Hawaiian"]
+ethnicities_list = ["Eurasian", "European", "Black", "White", "Latino", "None of your business", "Asian", "Hawaiian"]
 
 #example_genders.length.times do |i|
 num_of_santas = 350
 
 num_of_santas.times do |i|
-  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+  santas << Santa.new(gender_list.sample, ethnicities_list.sample)
   santas[i].age = prng.rand(0..140)
   puts "Santa # #{i+1} is a #{santas[i].gender}, #{santas[i].ethnicity}, #{santas[i].age} years old"
 end
