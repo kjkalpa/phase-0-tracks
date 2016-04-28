5b93485d2fbb0e3b4f9787c3929d0e5df77a77d4
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-28 11:35:36 -0500

    Pseudo code class

---
+++ ruby/solo.rb
@@ -0,0 +1,26 @@
+# Clown class
+#   Attributes
+#     Name - string
+#     Hair color - string
+#     Juggling_prop - string
+#     Shoe size - integer
+
+#   Methods
+#     Juggle: input - prop, name; output - message
+#     Honk: output - honk his horn
+#     Mime: input - name; output - box & message
+#     Pile_in_car - name; output - list of attributes getting into car
+
+
+
+
+
+
+
+
+
+
+
+
+
+
