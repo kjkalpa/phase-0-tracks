ef13ed61ccba727fc9f73b010a410aaf3dcf2b59
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-04-29 11:50:19 -0500

    Add 2 classes and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,13 +4,13 @@ module Shout
   end
 
   def yelling_happily(words)
-    words + ":)"
+    words + " :)"
   end
 
 end
 
 
-class Angry_driver
+class Car_driver
   include Shout
 end
 
@@ -19,6 +19,14 @@ class Angry_birds
 end
 
 
-
 # puts x = Shout.yelling_happily("Yes this worked ")
+
+driver = Car_driver.new
+bird = Angry_birds.new
+
+puts bird.yell_angrily("We're going to get you pigs")
+puts bird.yelling_happily("Yeah we knocked you down.")
+
+puts driver.yell_angrily("Get off the road you slow poke")
+puts driver.yelling_happily("Wow, I94 has no traffic.")
 
