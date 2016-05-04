0e3ee3aafca27a19789611ca5514dca9e18f446f
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2016-05-03 23:34:58 -0500

    Add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// assign var to string
+// pass string to function 
+// get length of string
+// loop backwards through string
+// add each letter to backwards_string
+// pass backwards string back to program
+
